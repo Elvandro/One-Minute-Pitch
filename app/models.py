d0b6999aa2bb698793c367018286b8eb0a6bc904
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-29 12:59:32 +0300

    This commit will create a migrations folder

--- app/models.py
+++ app/models.py
@@ -1,27 +1,27 @@
 from . import db
 
-class Users(db.Model):
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String)
-    email = db.Column(db.String)
-    password = db.Column(db.String)
+    username = db.Column(db.String(360))
+    email = db.Column(db.String(360))
+    password = db.Column(db.String(660))
 
     def __repr__(self):
-        return f'Users {self.username}'
+        return f'User {self.username}'
 
 
-class Categories(db.Model):
+class Category(db.Model):
     __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(360))
 
     def __repr__(self):
-        return f'Categories {self.name}'
+        return f'Category {self.name}'
 
-class Pitches(db.Model):
+class Pitch(db.Model):
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer, primary_key = True)
@@ -29,16 +29,15 @@ class Pitches(db.Model):
     pitch = db.Column(db.String)
 
     def __repr__(self):
-        return f'Pithches {self.pitch}'
+        return f'Pitch {self.pitch}'
 
-class Comments(db.Model):
+class Comment(db.Model):
     __tablename__ = 'comments'
 
     id =  db.Column(db.Integer, primary_key = True)
-    author = db.Column(db.String)
+    author = db.Column(db.String(360))
     pitch = db.Column(db.String)
     comment = db.Column(db.String)
 
-
     def __repr__(self):
-        return f'Comments {self.comment}
+        return f'Comment {self.comment}'
