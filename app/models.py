8c97aca509a17f84103bd2dddcb2e5734a03442f
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-30 13:13:58 +0300

    This commit will add to the initial file structure

--- app/models.py
+++ app/models.py
@@ -1,13 +1,18 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
     password = db.Column(db.String(255))
     # Defining the One to many relationship between a user and a pitch
     pitch = db.relationship('Pitch', backref="user", lazy='dynamic')
